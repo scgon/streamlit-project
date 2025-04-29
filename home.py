e39934c437247aa7063e340d649531c49a1c7023
scgon <200132506+scgon@users.noreply.github.com>
2025-04-29 10:31:09 -0700

    Add files via upload

--- home.py
+++ home.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title(" :red[Work in Progress]")
+st.title(":rainbow[Streamlit Project]")
+
+st.markdown(
+    """:small[:red[*If you want to reset in any game, reset button will be on the sidebar.]]  
+    :small[:red[**In most games, there will be in a submit button, but if there isn't,
+    press enter to submit.]]"""
+)
