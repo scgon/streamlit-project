9e763e670b037dd847717f406db2f2d3913883a9
scgon <200132506+scgon@users.noreply.github.com>
2025-05-01 10:41:48 -0700

    Update 0-issues_and_notes.py

--- pages/0-issues_and_notes.py
+++ pages/0-issues_and_notes.py
@@ -5,6 +5,7 @@ st.title(":rainbow[Issues and Notes]")
 with st.container(border=True):
     st.page_link("pages/5-guess_the_number.py", label=":green[**Guess the Number**]")
     st.write("Issue with reseting score, the score will not be reset properly. Will be fixed the next time I update this.")
+    st.write("Fix: reload the tab.")
 
 with st.container(border=True):
     st.page_link("pages/2-wordle.py", label=":green[**Wordle**]")
